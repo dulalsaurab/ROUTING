1c8386a76dbad92d4c8df67697f5611dd373439e
dulalsaurab <sdulal@memphis.edu>
2020-03-24 04:53:38 +0000

    some changes

--- scripts/automate_mini_ndn_experiments.sh
+++ scripts/automate_mini_ndn_experiments.sh
@@ -3,19 +3,19 @@
 #Run mini-ndn experiments
 #Automate minindn experiment
 #pwd-/home/mini-ndn/sdulal/automate_experiments/scripts/graphing-scripts
-root_path="/home/mini-ndn/sdulal"
+root_path="/home/mini-ndn/europa_bkp/mini-ndn/sdulal"
 exp_root="$root_path/automate_experiments"
 scripts_path="$exp_root/scripts"
 path_to_conf_file="$exp_root/topologies_bkp/3d.conf"
 graphing_script_home="$scripts_path/graphing-scripts"
-pings="300"
+pings="900"
 workd_dir="/mnt/ramdisk/"
 minindn_flags=""
 exp_name="pingall"
-strategy="asf"
-declare -a experiments=("rgh_pop_asf_n42_0_"$exp_name "rgh_pop_asf_n42_2_"$exp_name "rgh_pop_asf_n42_4_"$exp_name)
+strategy="asf/%FD%03/probing-interval~10000"
+#declare -a experiments=("rgh_pop_asf_n42_0_"$exp_name "rgh_pop_asf_n42_2_"$exp_name "rgh_pop_asf_n42_4_"$exp_name)
 
-#declare -a experiments=("ghrasf_vs_ls_br_0_pingall")
+declare -a experiments=("rgh_vs_ls_br_0_pingall_latestndndump")
 
 #This will remove the experiment folder, stop and clean mini-ndn, and stop nfd if it's running not stopped
 clean(){
@@ -47,13 +47,13 @@ run_mini_ndn(){
   nfaces=$2
   echo "Result Dir: " $resultDir "and nfaces: " $nface
   #pingall experiment best-route link-state for a given number of face
-  #clean
-  #minindn --faces $nfaces --nPings $pings --experiment pingall --no-cli $path_to_conf_file --work-dir=$workd_dir --result-dir $resultDir/ls
+  clean
+  minindn --faces $nfaces --nPings $pings --experiment pingall --no-cli $path_to_conf_file --work-dir=$workd_dir --result-dir $resultDir/ls
 
   #experiment with hyperbolic routing, asf strategy and for a given number of face
   clean
   minindn --faces $nfaces --nPings $pings --routing hr --strategy $strategy --experiment pingall --no-cli $path_to_conf_file --work-dir $workd_dir --result-dir=$resultDir/
-  #clean
+  clean
   sleep 10
 
   #create_sym_link $resultDir
@@ -66,12 +66,12 @@ echo $graphing_script_home
 
 for exp in "${experiments[@]}"; do
 
- nface=$(echo | awk -v r=$exp '{split(r,a,"_"); print a[5]}') #parse experiment list 
+ nface=$(echo | awk -v r=$exp '{split(r,a,"_"); print a[5]}') #parse experiment list
  _exp=$(echo | awk -v r=$exp '{split(r,a,"_"); print a[6]}')
  echo "nface:" $nface " Desc: " $exp " Experiment type: " $_exp
  run_mini_ndn $exp $nface $_exp
- #create_sym_link $exp
- #run_graphing_scrips $exp
- #mkdir -p $exp_root/results/$exp && cp $exp_root/hrls/* $_
+ create_sym_link $exp
+ run_graphing_scrips $exp
+ mkdir -p $exp_root/results/$exp && cp $exp_root/hrls/* $_
 
 done
